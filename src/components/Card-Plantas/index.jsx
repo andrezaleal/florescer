8b5d4208ec22df4fc5ad17f84f8b8404304ce126
Andreza Leal <andrezaleal12@gmail.com>
2023-11-02 00:34:52 -0300

    edit card de plantas

---
+++ src/components/Card-Plantas/index.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import {
+  Typography,
+  Row,
+  Col,
+  Image,
+  Card,
+} from 'antd';
+import './styles.css';
+import cactus from "../../assets/cacto.png"
+import cardVerde from "../../assets/crad-medio-verde.png"
+import minhasPlantasIcon from '../../assets/minhas-plantas-icon.svg'
+const CardPlanta = ({ nome }) => {
+  const { Text } = Typography;
+  return (
+    <>
+      <Row className='container_item' style={{ alignContent: 'center' }}>
+        <Col>
+          <Card className="card-catalogo-style" bordered={false} style={{ backgroundImage: `url(${cardVerde})`, color: "FFFFFF",}}>
+
+            <Image
+              src={minhasPlantasIcon}
+              preview={false}
+              className='icon-catalogo'
+            />
+            <Image
+              src={cactus}
+              preview={false}
+              className='img-catalogo'
+            />
+          </Card>
+          <Row className='container_item' style={{ alignContent: 'center' }}>
+            <Col>
+            <Text style={{ color: '#6D7970'}} className='nome-planta'>{nome}</Text>
+            </Col>
+            </Row>
+        </Col>
+      </Row>
+    </>
+  );
+}
+
+export default CardPlanta;
