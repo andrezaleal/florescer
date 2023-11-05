0a80e930b4b746c7f46158867e06108befcee23a
Andreza Leal <andrezaleal12@gmail.com>
2023-11-05 01:39:37 -0300

    adicao da secao minhas plantas

--- src/pages/Minhas-Plantas/index.jsx
+++ src/pages/Minhas-Plantas/index.jsx
@@ -1,108 +1,141 @@
-import React from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 import {
-    Layout,
-    Typography,
-    Input,
-    Row,
-    Col,
-    Button,
-    Image,
-    Card,
-    Tooltip,
+  Layout,
+  Typography,
+  Input,
+  Row,
+  Col,
+  Button,
+  Tooltip,
+  Image,
+  Spin,
 } from 'antd';
 import { useHistory, Link } from 'react-router-dom';
 import './styles.css';
 import { ArrowLeftOutlined, SearchOutlined, PlusOutlined } from '@ant-design/icons';
-import cactus from "../../assets/cacto.png";
-import cardVerde from "../../assets/crad-medio-verde.png";
-import minhasPlantasIcon from "../../assets/minhas-plantas-icon.svg";
 import "antd/dist/antd.css";
+
+import { getDocs, collection, doc, where, query } from 'firebase/firestore';
+import { db } from '../../services/firebaseConnections';
+import { AuthContext } from '../../services/auth';
+
+import CardPlanta from '../../components/Card-Plantas';
+import folha from "../../assets/folha.svg";
+import MenuComponent from '../../components/MenuComponent';
+
+
 
 const { Title, Text } = Typography;
 const { Content } = Layout;
 
+const allPlants = collection(db, "plantas");
 const MinhasPlantas = () => {
-    const history = useHistory();
-    return (
-        <Layout className="layout" >
-            <Row className='container_item' style={{ justifyContent: 'flex-start', marginBottom: '2rem', marginTop: '3rem'}}>
-                <Col span={6}>
-                    <Button type='link' style={{ color: '#6D7970' }} onClick={history.goBack}>
-                        <ArrowLeftOutlined style={{ fontSize: '26px', padding: 0 }} />
-                    </Button>
-                </Col>
-                <Col span={18}>
-                    <Title className='titulo'  style={{ margin: "0", display: 'flex' }}>Minhas Plantas</Title>
-                </Col>
-            </Row>
-            <Content className="site-layout-content" style={{ display: 'block', padding: 0 }} >
-                <Row className='container_item'>
-                    <Col>
-                        <Input
-                            placeholder="Pesquisar"
-                            style={{
-                                width: '85vw',
-                                background: 'none',
-                            }}
-                            suffix={<SearchOutlined />}
-                            size="large"
-                        />
-                    </Col>
-                    <Row className='container_step'>
-                        <Row className='container_item' style={{ alignContent: 'center' }}>
-                            <Col>
-                                <Card className="card-catalogo-style" bordered={false} style={{ backgroundImage: `url(${cardVerde})`, color: "FFFFFF", margin: '10px 0px 0px 0' }}>
+  const { user } = useContext(AuthContext);
+  const userPlantsQuery = query(allPlants, where("userId", "==", user.uid));
+  const [plantas, setPlantas] = useState([]);
+  const [loadPlantas, setLoadPlantas] = useState(true);
+  const [searchTerm, setSearchTerm] = useState('');
 
-                                    <Image
-                                        src={minhasPlantasIcon}
-                                        preview={false}
-                                        className='icon-catalogo'
-                                    />
-                                    <Image
-                                        src={cactus}
-                                        preview={false}
-                                        className='img-catalogo'
-                                    />
-                                </Card>
-                                <Card className="card-catalogo-style" bordered={false} style={{ backgroundImage: `url(${cardVerde})`, color: "FFFFFF", margin: '10px 0px 0px 0' }}>
-                                    <Image
-                                        src={minhasPlantasIcon}
-                                        preview={false}
-                                        className='icon-catalogo'
-                                    />
-                                    <Image
-                                        src={cactus}
-                                        preview={false}
-                                        className='img-catalogo'
-                                    />
-                                </Card>
-                            </Col>
-                        </Row>
-                        <Row className='container_item' style={{ margin: '0' }}>
-                            <Col>
-                                <Card className="card-categoria" >
-                                    <Text style={{ color: '#6D7970' }}>Cacto juliette</Text>
-                                </Card>
+  const history = useHistory();
 
-                                <Card className="card-categoria">
-                                    <Text style={{ color: '#6D7970' }}>Cacto juliette</Text>
-                                </Card>
-                            </Col>
-                        </Row>
-                    </Row>
-                    <Tooltip title="add">
-                        <Link to="/adicionar planta">
-                            <Button className='button-add' type="primary" size="large" shape="circle" icon={<PlusOutlined className='icon-add' />} />
-                        </Link>
-                    </Tooltip>
+  useEffect(() => {
+    async function loadPlantas() {
+      const querySnapshot = await getDocs(userPlantsQuery)
+        .then((snapshot) => {
+          const lista = [];
+          snapshot.forEach((doc) => {
+            lista.push({
+              id: doc.id,
+              nome: doc.data().nomePlanta,
+              idade: doc.data().idade,
+              createdAt: doc.data().createdAt,
+            })
+          })
+          if (snapshot.docs.size === 0) {
+            console.log("Nenhuma Planta Encontrada")
+            setLoadPlantas(false);
+            return;
+          }
+          lista.sort((a, b) => b.createdAt - a.createdAt);
+          setPlantas(lista);
+          setLoadPlantas(false);
+        })
+        .catch((error) => {
+          console.log(error);
+          setLoadPlantas(false);
+        })
+       
+    }
+    loadPlantas()
+  }, []);
 
-                </Row>
+  const filteredPlantas = plantas.filter((planta) => {
+    return planta.nome.toLowerCase().includes(searchTerm.toLowerCase());
+  });
 
-            </Content >
+  return (
+    <Layout className="layout" >
+      <Row className='container_item' style={{ justifyContent: 'flex-start', marginBottom: '2rem', marginTop: '3rem' }}>
+        <Col span={6}>
+          <Button type='link' style={{ color: '#6D7970' }} onClick={history.goBack}>
+            <ArrowLeftOutlined style={{ fontSize: '26px', padding: 0 }} />
+          </Button>
+        </Col>
+        <Col span={18}>
+          <Title className='titulo' style={{ margin: "0", display: 'flex' }}>Minhas Plantas</Title>
+        </Col>
+      </Row>
+      <Content className="site-layout-content" style={{ display: 'block', padding: 0 }} >
+        <Row className='container_item'>
+          <Col>
+            <Input
+              placeholder="Pesquisar"
+              value={searchTerm}
+              style={{
+                width: '85vw',
+                background: 'none',
+              }}
+              onChange={(e) => setSearchTerm(e.target.value)}
+              suffix={<SearchOutlined />}
+              size="large"
+            />
+          </Col>
+          <Row className='container_step' style={{ flexDirection: 'initial', justifyContent: 'start' }}>
+            {
+              loadPlantas ?
+                <Spin tip="Carregando..." size="large" alignitems={'center'} className='loading' />
+                : <>
+                  {plantas.length < 1
+                    ?
+                    <div className='mensagem-jardim'>
+                      <Image
+                        src={folha}
+                        preview={false}
+                        className='img-folha'
+                      />
+                      <Text style={{ color: '#6D7970' }}>Você ainda não adicinou nenhuma plantinha ao seu jardim..</Text>
+                    </div>
+                    :
+                    filteredPlantas.map((item, index) => (
+                      <div key={index}>
+                        <Link to={`/planta-id/${item.id}`}><CardPlanta nome={item.nome} /></Link>
+                      </div>
+                    ))}
 
+                </>
+            }
 
-        </Layout >
-    );
+          </Row>
+        </Row>
+        <Tooltip title="add">
+            <Link to="/adicionar planta">
+              <Button className='button-add' type="primary" size="large" shape="circle" icon={<PlusOutlined className='icon-add' />} />
+            </Link>
+          </Tooltip>
+      </Content >
+      <MenuComponent/>
+    </Layout >
+  );
 };
 
 
